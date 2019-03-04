2df6e6ad4705be08504bafff8bf5b9de450c31fd
Dave Xanatos <xanatos@xanatos.com>
2019-03-03 20:42:40 -0500

    Updates to non-proprietary scripts

--- SpeechCenter.py
+++ SpeechCenter.py
@@ -1,5 +1,4 @@
-# python SpeechCenter.py
-# Connects to port 5555 using ZeroMQ, receives speech commands from other scripts
+# python SpeechCenter.py - Connects to port 5555 using ZeroMQ, receives speech commands from other scripts
 
 import time
 import zmq
@@ -7,40 +6,29 @@ import os
 import re
 
 # Open Host Behavior Matrix on Server Portal Folder:
-#Host = open('/var/www/html/HostBuilds/MAEVE.txt', "r")
-#HostLines = Host.readlines()
-#Host.close
+Host = open('/var/www/html/HostBuilds/ACTIVEHOST.txt', "r")  # On Host Processor
+HostLines = Host.readlines()
+Host.close
 
-#HostIDparts = HostLines[0].split("|")
-
-#HostName = HostIDparts[0]
-#HostSex = HostIDparts[1]
-#HostAge = HostIDparts[2]
-#HostOccupation = HostIDparts[3]
-#HostID = HostIDparts[4]
-
-#if HostSex == "M":
-#    baseVoice = "D"
-#elif HostSex == "F":
-#    baseVoice = "B"
-#else:
-#    baseVoioce = "V"
+HostIDparts = HostLines[0].split("|")  # HostLines[0] contains the base self attributes, [1] contains system parameters for OpenCV, etc., [2] - [21] contain the matrix attributes
+V = HostIDparts[5]  # Host Voice
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
 def set_voice(V,T):
+    audioFile = "/home/pi/Desktop/HOSTCORE/tmp.wav"
     if V == "A":
-        os.system("swift -n Allison -o tmp.wav "+T+" && aplay -D plughw:1,0 tmp.wav") 
+        os.system("swift -n Allison -o " + audioFile + " " +T+ " && aplay -D plughw:1,0 " + audioFile) 
     if V == "B":
-        os.system("swift -n Belle -o tmp.wav "+T+" && aplay -D plughw:1,0 tmp.wav") 
+        os.system("swift -n Belle -o " + audioFile + " " +T+ " && aplay -D plughw:1,0 " + audioFile) 
     if V == "C":
-        os.system("swift -n Callie -o tmp.wav "+T+" && aplay -D plughw:1,0 tmp.wav") 
+        os.system("swift -n Callie -o " + audioFile + " " +T+ " && aplay -D plughw:1,0 " + audioFile) 
     if V == "D":
-        os.system("swift -n Dallas -o tmp.wav "+T+" && aplay -D plughw:1,0 tmp.wav") 
+        os.system("swift -n Dallas -o " + audioFile + " " +T+ " && aplay -D plughw:1,0 " + audioFile) 
     if V == "V":
-        os.system("swift -n David -o tmp.wav "+T+" && aplay -D plughw:1,0 tmp.wav") 
+        os.system("swift -n David -o " + audioFile + " " +T+ " && aplay -D plughw:1,0 " + audioFile) 
 
 pronunciationDict = {'Maeve':'Mayve','Mariposa':'May-reeposah','Lila':'Lie-la','Trump':'Ass hole'}
 
@@ -51,12 +39,13 @@ def adjustResponse(response):     # Adjusts spellings in verbal output string on
     return response
 
 SpeakText="Speech center connected and online."
-set_voice("B",SpeakText) # Cepstral  Voices: A = Allison; B = Belle; C = Callie; D = Dallas; V = David;
+set_voice(V,SpeakText) # Cepstral  Voices: A = Allison; B = Belle; C = Callie; D = Dallas; V = David;
 print(SpeakText)
 
 while True:
-    SpeakText = socket.recv().decode('utf-8') # Get rid of the b' in front of teh string
+    SpeakText = socket.recv().decode('utf-8') # .decode gets rid of the b' in front of the string
+    SpeakText = re.sub("'", "", SpeakText)
     SpeakTextX = adjustResponse(SpeakText)    # Run the string through the pronunciation dictionary
-    set_voice("B",SpeakTextX)
+    set_voice(V,SpeakTextX)
     print("Received request: %s" % SpeakText)
-    socket.send_string(str(SpeakText))  # Send data back to source for confirmation
+    socket.send_string(str(SpeakText))        # Send data back to source for confirmation
